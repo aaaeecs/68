451a77212797fad078a87ccef91d2c6031392d87
Adam T <adamtaranissi2021@u.northwestern.edu>
2019-02-19 16:52:50 -0600

    New Pages
    Adding new pages - filter page - for project 6

---
+++ filterpage.js
@@ -0,0 +1,5 @@
+function updateActive() {
+  // document.getElementById("SignIn").classList.add("active");
+  // document.getElementById("Home").classList.remove('active');
+  document.getElementById("SingIn").className = "active";
+}
